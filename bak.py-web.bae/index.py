b9a26cb738f6ff5f7dcf63423d1d428799daece8
dezhichen <c8d8z8@gmail.com>
2016-01-12 11:27:45 +0800

    一堆烂代码

---
+++ bak.py-web.bae/index.py
@@ -0,0 +1,16 @@
+﻿#-*- coding:utf-8 -*-
+
+import os
+import sys
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'weibo.settings'
+
+path = os.path.dirname(os.path.abspath(__file__)) + '/weibo'
+if path not in sys.path:
+    sys.path.insert(1, path)
+    
+from django.core.handlers.wsgi import WSGIHandler
+
+from bae.core.wsgi import WSGIApplication
+
+application = WSGIApplication(WSGIHandler())
