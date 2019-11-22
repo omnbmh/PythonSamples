39caf12a5ddcc410a79555fb4fe3467920368408
Cobra <c8d8z8@gmail.com>
2019-11-22 17:15:13 +0800

    test image

--- test_Image.py
+++ test_Image.py
@@ -6,7 +6,10 @@ Python 图像处理库
 '''
 
 from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
 import numpy as np
+import random
 
 
 def show_image(img_path):
@@ -35,7 +38,35 @@ def resize_image(img_path):
     new_image.save("test_image_resize_L.jpeg")
 
 
+def generate_captcha(weight, hight, str, bg_color, line_count, point_count):
+    # 生成一个Image 指定大小 和 背景色
+    image = Image.new('RGB', (weight, hight), bg_color)
+
+    # 来一个画笔
+    draw = ImageDraw.Draw(image)
+    font = ImageFont.truetype("fonts/msyh.ttf", size=36)
+
+    # 把字写到Image上
+    draw.text((12, 0), str, (0, 0, 0), font=font)
+
+    if line_count:
+        for i in range(line_count):
+            x1 = random.randint(0, weight)
+            x2 = random.randint(0, weight)
+            y1 = random.randint(0, hight)
+            y2 = random.randint(0, hight)
+            draw.line((x1, y1, x2, y2), fill=(123, 234, 123))
+
+    if point_count:
+        for i in range(point_count):
+            x = random.randint(0, weight)
+            y = random.randint(0, hight)
+            draw.arc((x, y, x + 2, y + 2), 0, 90, fill=(234, 123, 234))
+    image.save(str + '.png', 'png')
+
+
 if __name__ == '__main__':
     img_path = 'test_image.jpeg'
     # show_image(img_path)
-    resize_image(img_path)
+    # resize_image(img_path)
+    generate_captcha(48, 48, 'A', (255, 255, 255), 0, 0)
